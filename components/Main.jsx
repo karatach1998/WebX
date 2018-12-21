a691426eee3d5e1a940613ef989b9e00bcd56baa
Sergey Karatach <karatach1998@yandex.ru>
2018-12-22 02:24:03 +0300

    Refactor Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,29 +1,14 @@
 const React = require('react');
 const { Link } = require('react-router-dom');
-const Modal = require('react-modal');
 const axios = require('axios');
 
 const Preload = require('./Preload');
 const Header = require('./Header');
 const Boards = require('./Boards');
 
-const modalStyles = {
-    content : {
-        top                   : '50%',
-        left                  : '50%',
-        right                 : 'auto',
-        bottom                : 'auto',
-        marginRight           : '-50%',
-        transform             : 'translate(-50%, -50%)',
-    },
-    overlay: {
-        zIndex                : 10
-    }
-};
-
-Modal.setAppElement('#root');
-
 class Main extends React.Component {
+    BG_COLOR = "#237FC2";
+
     state = {
         isSubmitting: false,
         groups: null
@@ -48,20 +33,15 @@ class Main extends React.Component {
         });
     }
 
-    handleCreateBoard(title) {
-        console.log('fjak;');
-        axios.post('/api/boards', { title });
-    }
-
     render() {
         console.log(this.props);
         if (this.state.isSubmitting || !this.state.groups) {
-            return <Preload color={`#237FC2`}/>;
+            return <Preload color={this.BG_COLOR}/>;
         }
 
         return (
             <div>
-                <Header bgcolor="#237FC2" userinitials="CK" onCreateBoard={this.handleCreateBoard} />
+                <Header bgcolor={this.BG_COLOR} userinitials="CK" />
                 <Boards key="boards" groups={this.state.groups} />
             </div>
         );
